1ae8f4a6ac4fbcbed5f2df7ef5cf14ec93542a60
sandajama <sandajama29@gmail.com>
2023-06-21 16:26:09 +0300

    answered

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+const makeCapital=(sentance)=>{
+    let str = sentance.split(" ")
+    for (let i=0; i<str.length; i++){
+        str[i]=str[i][0].toUpperCase() + str[i].substring(1)
+    }
+
+return str.join(" ")
+}
+
+console.log(makeCapital("my name is ali")) //➞ "My Name Is Ali"
+console.log(makeCapital("fikrcamp Bootcamp")) //➞ "Fikrcamp Bootamp"
+console.log(makeCapital("i live In Paris")) //➞ "I Live In Paris"
